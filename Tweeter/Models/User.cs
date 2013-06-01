ef76b445c8484ae65a9d4c1db895fea7b9bfd3d2
Rod Hoover <rodchoover@gmail.com>
2013-06-01 09:59:02 -0500

    ran scaffolding for User

---
+++ Tweeter/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Tweeter.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string avatar { get; set; }
+        public string about { get; set; }
+        public string name { get; set; }
+        
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
